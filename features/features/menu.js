d42b978fdb733e3dedd31bfe920178ce2cf76ebd
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-24 17:29:55 +0800

    Update menu.js

--- features/features/menu.js
+++ features/features/menu.js
@@ -1,1 +1,33 @@
+async function show(sock,user){
 
+await sock.sendMessage(
+user,
+{
+text:
+`╭─ ⋆｡˚✩ 𝓝𝓩𝓢𝓣𝓞𝓡𝓔 𝓜𝓮𝓷𝓾 ✩˚｡⋆ ─╮
+
+Silakan pilih layanan:
+
+┃ 🛒 Produk
+┃ 💰 Harga
+┃ 📌 Rules Grup
+┃ 📦 Cara Order
+┃ 🧾 Format Order
+┃ 💳 Payment
+┃ 👤 Admin
+┃ 🎀 Reseller
+┃ 📣 Promo
+┃ ❓ Bantuan
+
+Ketik nama menu yang ingin dilihat.
+
+╰────────────────╯`
+}
+);
+
+}
+
+
+module.exports={
+show
+};
