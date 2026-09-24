3a570ab8b2130b903c44fa3c5c4363d02f861dfd
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-24 17:32:25 +0800

    Create reseller.js

---
+++ features/reseller.js
@@ -0,0 +1,34 @@
+async function info(sock,user){
+
+await sock.sendMessage(
+user,
+{
+text:
+`╭─ ⋆｡˚✩ 𝓡𝓮𝓼𝓮𝓵𝓵𝓮𝓻 ✩˚｡⋆ ─╮
+
+PROGRAM RESELLER NZSTORE ✨
+
+┃ • Bisa menjual produk NZSTORE
+┃ • Tanpa modal awal
+┃ • Mendapat ID reseller
+┃ • Bisa mengikuti pencapaian bulanan
+┃ • Tidak ada target wajib
+
+Pencapaian Top Reseller:
+
+🥇 Top 1 — 200K
+🥈 Top 2 — 150K
+🥉 Top 3 — 100K
+
+Ranking berdasarkan pencapaian tertinggi.
+
+╰────────────────╯`
+}
+);
+
+}
+
+
+module.exports={
+info
+};
