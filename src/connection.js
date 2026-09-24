e635ab45d24a0751450bf5bb9c0d00e273371956
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-24 17:09:41 +0800

    Create connection.js

---
+++ src/connection.js
@@ -0,0 +1,11 @@
+const { default: makeWASocket } = require("@whiskeysockets/baileys");
+
+async function startBot() {
+
+    console.log("NZstore Bot Starting...");
+
+}
+
+module.exports = {
+    startBot
+};
