10e1d15aff5c5ea3c319a58cd275b1029d2a464d
r9pjww7rcs-beep <r9pjww7rcs@privaterelay.appleid.com>
2026-09-24 17:19:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
-    botName: "NZstore Bot",
-    owner: "085828626140",
-    prefix: "."
+    botName: process.env.BOT_NAME,
+    owner: process.env.OWNER_NUMBER,
+    prefix: process.env.PREFIX,
+    database: process.env.DATABASE_NAME,
+
+    admins: [
+        process.env.OWNER_NUMBER
+    ]
 };
